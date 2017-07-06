a86c77f85695b3c09abec8d632dc5455d28a5d68
zhangdongpo <zdp930128@gmail.com>
2017-07-06 18:43:18 +0800

    添加数据源

---
+++ SFTableView/SFTableViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  SFTableViewDataSource.h
+//  SFTableView
+//
+//  Created by zhangdongpo on 2017/7/6.
+//  Copyright © 2017年 张东坡. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFTableViewDataSource : NSObject
+@property (nonatomic, strong) NSArray *sectionArray;
+@end
---
+++ SFTableView/SFTableViewDataSource.m
@@ -0,0 +1,13 @@
+//
+//  SFTableViewDataSource.m
+//  SFTableView
+//
+//  Created by zhangdongpo on 2017/7/6.
+//  Copyright © 2017年 张东坡. All rights reserved.
+//
+
+#import "SFTableViewDataSource.h"
+
+@implementation SFTableViewDataSource
+
+@end
